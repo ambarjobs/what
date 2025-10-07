bef9216a7dede762ce491d52be5cf0c47e94c8d8
Armando M. Baratti <ambarjobs@gmail.com>
2025-10-07 11:42:57 -0300

    Solicitação do nome do comando na adição de comandos se ele não foi informado. Verificação se o comando foi realmente gravado na base de dados.

--- src/what/actions_handling.clj
+++ src/what/actions_handling.clj
@@ -46,14 +46,17 @@
   "Adiciona um novo comando na base de dados."
   [args]
   (let [{:keys [opts]} args
-        {command-name :command} opts
+        {raw-command-name :command} opts
+        command-name (or raw-command-name (utils/prompted-input "Digite o nome do comando: "))
         command-description (utils/prompted-input "Descrição do comando: ")
         raw-command-help (utils/prompted-input "Help do comando (man (default) | --help | --outra-opção): ")
         command-help (if (= raw-command-help "") "man" raw-command-help)
         raw-util-name (utils/prompted-input "Nome do utilitário (se diferente do nome do comando): ")
         util-name (if (= raw-util-name "") nil raw-util-name)]
     (try
-      (db/add-command command-name command-description command-help util-name)
+      (let [result (db/add-command command-name command-description command-help util-name)]
+        (when (not= (:rows-affected result) 1)
+          (println (format "\nNão foi possível adicionar o comando [%s] à base de dados." command-name))))
       (catch Exception except
         (if (= (:cause (Throwable->map except)) "UNIQUE constraint failed: command.command")
           (println (format "\nO comando [%s] já existe na base de dados." command-name))
